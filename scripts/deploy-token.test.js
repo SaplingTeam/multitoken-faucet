f6e9c6a8a251d1c10d4bcb53b0e3411a40988fdc
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-08-24 16:17:10 +0600

    configurable test token

--- scripts/deploy-token.test.js
+++ scripts/deploy-token.test.js
@@ -3,13 +3,14 @@ const { ethers } = require("hardhat");
 
 async function main() {
     [deployer, ...addrs] = await ethers.getSigners();
+    const arguments = require('./arguments-test-token.js');
   
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    TestUSDC = await ethers.getContractFactory("TestUSDC");
-    tokenContract = await TestUSDC.deploy();
-    console.log("TestUSDC address:", tokenContract.address);
+    TestToken = await ethers.getContractFactory("TestToken");
+    tokenContract = await TestToken.deploy(...arguments);
+    console.log("Test token address:", tokenContract.address);
   }
   
   main()
